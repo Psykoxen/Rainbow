06d9652e242afc062ffe4b24fdc5c76117936bad
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-03-06 14:12:41 +0100

    Initial Files

---
+++ app/src/main/java/fr/rainbow/ui/map/MapFragment.kt
@@ -0,0 +1,42 @@
+package fr.rainbow.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import fr.rainbow.databinding.FragmentMapBinding
+
+class MapFragment : Fragment() {
+
+    private var _binding: FragmentMapBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val mapViewModel =
+            ViewModelProvider(this).get(MapViewModel::class.java)
+
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textMap
+        mapViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
