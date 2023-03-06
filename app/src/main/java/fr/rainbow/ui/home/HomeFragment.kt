3ed427db49d1336bc190d1a7a5f0d1536652fdd6
Larcherbc <jules.rbc@gmail.com>
2023-03-06 21:52:04 +0100

    Add gps to HomeFragment.kt

--- app/src/main/java/fr/rainbow/ui/home/HomeFragment.kt
+++ app/src/main/java/fr/rainbow/ui/home/HomeFragment.kt
@@ -1,34 +1,48 @@
 package fr.rainbow.ui.home
 
 import WeatherData
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
-import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.*
 import com.google.gson.Gson
-import fr.rainbow.Manifest
 import fr.rainbow.R
 import fr.rainbow.databinding.FragmentHomeBinding
 import kotlinx.android.synthetic.main.fragment_home.*
-import java.io.IOException
 import okhttp3.*
+import java.io.IOException
 import java.time.LocalDateTime
 
 class HomeFragment : Fragment() {
     private val client = OkHttpClient()
     private var _binding: FragmentHomeBinding? = null
+
+    //GPS
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private var longitude: Double = 0.0
+    private var latitude: Double = 0.0
+    private val interval: Long = 10000 // 10seconds
+    private val fastestInterval: Long = 5000 // 5 seconds
+    private lateinit var mLastLocation: Location
+    private lateinit var mLocationRequest: LocationRequest
+    private val requestPermissionCode = 999
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -50,6 +64,23 @@ class HomeFragment : Fragment() {
         }
         requestMainSection("https://api.open-meteo.com/v1/forecast?latitude=43.61&longitude=3.88&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,weathercode,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,uv_index_max&timezone=Europe%2FBerlin")
 
+
+        //GPS
+        fusedLocationProviderClient = this.activity?.let {
+            LocationServices.getFusedLocationProviderClient(
+                it
+            )
+        }
+        mLocationRequest = LocationRequest.create()
+        val locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showAlertMessage()
+        }
+        this.activity?.let { checkForPermission(it) }
+        startLocationUpdates()
+
+
         return root
 
     }
@@ -57,6 +88,11 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationProviderClient?.removeLocationUpdates(mLocationCallback)
     }
 
     fun requestMainSection(url: String) {
@@ -142,6 +178,107 @@ class HomeFragment : Fragment() {
             96 -> icon.setImageResource(R.drawable.ic_weather_code_96_99)
             99 -> icon.setImageResource(R.drawable.ic_weather_code_96_99)
             else -> icon.setImageResource(R.drawable.ic_weather_code_0)
+        }
+    }
+
+    //GPS
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation
+            Log.d("MainActivity", "callback: $latitude $longitude")
+            locationResult.lastLocation?.let { locationChanged(it) }
+            latitude = locationResult.lastLocation?.latitude!!
+            longitude = locationResult.lastLocation?.longitude!!
+            Log.d("GPS","latitude : $latitude longitude : $longitude")
+        }
+    }
+
+    private fun startLocationUpdates() {
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = interval
+        mLocationRequest.fastestInterval = fastestInterval
+
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(mLocationRequest)
+        val locationSettingsRequest = builder.build()
+        val settingsClient = this.activity?.let { LocationServices.getSettingsClient(it) }
+        if (settingsClient != null) {
+            settingsClient.checkLocationSettings(locationSettingsRequest)
+        }
+
+        fusedLocationProviderClient = this.activity?.let {
+            LocationServices.getFusedLocationProviderClient(
+                it
+            )
+        }
+
+        if (this.activity?.let {
+                ActivityCompat.checkSelfPermission(
+                    it, android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED && this.activity?.let {
+                ActivityCompat.checkSelfPermission(
+                    it, android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            } != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallback,
+            Looper.myLooper()!!)
+    }
+
+    private fun checkForPermission(context: Context) {
+        if (context.checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+            return
+        } else {
+            this.activity?.let {
+                ActivityCompat.requestPermissions(
+                    it, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    requestPermissionCode)
+            }
+            return
+        }
+    }
+
+    private fun showAlertMessage() {
+        val builder = AlertDialog.Builder(this.activity)
+        builder.setMessage("The location permission is disabled. Do you want to enable it?")
+            .setCancelable(false)
+            .setPositiveButton("Yes") { _, _ ->
+                startActivityForResult(
+                    Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    , 10)
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.cancel()
+
+            }
+        val alert: AlertDialog = builder.create()
+        alert.show()
+    }
+
+    fun locationChanged(location: Location) {
+        mLastLocation = location
+        longitude = mLastLocation.longitude
+        latitude = mLastLocation.latitude
+        Log.d("GPS", "function: $latitude $longitude")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestPermissionCode) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            } else {
+                Toast.makeText(this.activity, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
