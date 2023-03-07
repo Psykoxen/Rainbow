59fafb2610269d119537169d6492236ed26d9e19
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-03-07 22:18:16 +0100

    Fixing Detailed Activity

--- app/src/main/java/fr/rainbow/ui/detailed/DetailedActivity.kt
+++ app/src/main/java/fr/rainbow/ui/detailed/DetailedActivity.kt
@@ -1,9 +1,19 @@
 package fr.rainbow.ui.detailed
 
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import fr.rainbow.databinding.ActivityDetailedBinding
+import fr.rainbow.databinding.ActivityMainBinding
 
 
 class DetailedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailedBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
