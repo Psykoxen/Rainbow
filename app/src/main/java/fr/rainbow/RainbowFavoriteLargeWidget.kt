bc8810ae32fa499e4fa180d2c9259d8cd71f498b
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-03-20 22:57:24 +0100

    Fixing Widget

--- app/src/main/java/fr/rainbow/RainbowFavoriteLargeWidget.kt
+++ app/src/main/java/fr/rainbow/RainbowFavoriteLargeWidget.kt
@@ -1,10 +1,12 @@
 package fr.rainbow
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.os.Handler
 import android.widget.RemoteViews
 import fr.rainbow.MainActivity.Companion.favorites
 import fr.rainbow.functions.Functions
@@ -13,23 +15,23 @@ import fr.rainbow.functions.Functions
  * Implementation of App Widget functionality.
  */
 class RainbowFavoriteLargeWidget : AppWidgetProvider() {
+
+    private val handler = Handler()
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
-    }
-
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+        handler.postDelayed(
+            { onUpdate(context, appWidgetManager, appWidgetIds) }, 30000
+        )
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        handler.removeCallbacksAndMessages(null)
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -43,7 +45,6 @@ internal fun updateAppWidget(
     appWidgetId: Int
 ) {
 
-    // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.rainbow_favorite_large_widget)
     views.setTextViewText(R.id.widgetText,favorites[0].name)
     views.setTextViewText(R.id.widgetTemp,favorites[0].weatherData!!.hourly.temperature_2m.get(
@@ -53,7 +54,5 @@ internal fun updateAppWidget(
     Functions.updatingWeatherWidgetIc(
         views,
         favorites[0].weatherData!!.hourly.weathercode[Functions.findCurrentSlotHourly(favorites[0])])
-
-    // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
