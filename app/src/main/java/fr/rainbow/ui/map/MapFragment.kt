d969e3dba3de8bd1760581004b38a5b2e62e975f
Niskko <niskko.yt@gmail.com>
2023-03-20 20:32:45 +0100

    Fix: Bug RequireContext

--- app/src/main/java/fr/rainbow/ui/map/MapFragment.kt
+++ app/src/main/java/fr/rainbow/ui/map/MapFragment.kt
@@ -105,35 +105,20 @@ class MapFragment : Fragment() {
                     val gson = Gson()
                     val weatherCode = gson.fromJson(response.body()?.string(), MapsData::class.java)
                     activity?.runOnUiThread {
-                        addMarkers2(city, googleMap, weatherCode.hourly.weathercode[findCurrentSlotHourly(weatherCode)])
+                        addMarkers(city, googleMap, weatherCode.hourly.weathercode[findCurrentSlotHourly(weatherCode)])
                     }
                 }
             })
         }
     }
 
-    private fun addMarkers2(city: Cities, googleMap: GoogleMap, weatherCode: Int) {
+    private fun addMarkers(city: Cities, googleMap: GoogleMap, weatherCode: Int) {
         val marker = googleMap.addMarker(
             MarkerOptions()
                 .title(city.name)
                 .position(city.latLng)
         )
-        updatingWeatherBmpIc(marker, requireContext(), weatherCode)
-    }
-    private fun addMarkers(googleMap: GoogleMap) {
-        cities.getAll().forEach() { place ->
-            val marker = googleMap.addMarker(
-                MarkerOptions()
-                    //.title(place.name)
-                    .position(place.latLng)
-            )
-            Log.d("DEBUGMAP", favorites.find { it.name == place.name }.toString())
-            if(favorites.find { it.name == place.name } != null) {
-                val fav = favorites.find { it.name == place.name }
-                updatingWeatherBmpIc(marker, requireContext(), fav!!.weatherData!!.hourly.weathercode[findCurrentSlotHourly(fav)])
-            }
- }
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cities.get(0).latLng, 8f))
+        context?.let { updatingWeatherBmpIc(marker, it, weatherCode) }
     }
 
     override fun onDestroyView() {
